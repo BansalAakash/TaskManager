04d9325c5cf679f0964d3b555b4074b4c691bc65
Aakash Bansal <aakash6025@gmail.com>
2022-05-26 21:32:08 +0530

    Added the upload avatar feature

--- src/index.js
+++ src/index.js
@@ -13,21 +13,7 @@ app.use(taskRouter)
 
 console.clear()
 
+
 app.listen(port, () => {
     console.log('Listening on port', port)
 })
-
-
-const Task = require('./models/task')
-const User = require('./models/user')
-
-const main = async () => {
-    // const task = await Task.findById('628e06b3e2e34fae8868a5fe')
-    // await task.populate('createdBy')        
-    // console.log(task.createdBy)
-
-    const user = await User.findById('628e0445ef0deaf50381091c')
-    await user.populate('tasks')
-    console.log(user.tasks)
-}
-// main()
--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -2,6 +2,8 @@ const express = require('express')
 const router = new express.Router()
 const User = require('../models/user')
 const auth = require('../middleware/auth')
+const multer = require('multer')
+const sharp = require('sharp')
 
 router.post('/users', async ({body} = {}, res) => {
     console.log(body)
@@ -133,6 +135,55 @@ router.patch('/users/me', auth, async (req, res) => {
     }catch(error){
         console.log(error)
         res.status(400).send(error)
+    }
+})
+
+const upload = multer({
+    // dest: 'avatars',                                     We don't want to save the image to the filesystem anymore as it will be wiped out after every new release, instead we will store it in mongo using model
+    limits: {
+        fileSize: 1000000                                   //1MB
+    },
+    fileFilter(req, file, cb) {
+        if(!file.originalname.match(/\.(jpg|jpeg|png)$/)){        //try regex at regex101.com
+            cb(new Error('File must be picture'))
+        }
+        cb(undefined, true)
+
+        // cb(new Error('Error!'))                          //3 ways of using fileFilter, this one throws error and rejects
+        // cb(undefined, true)                              //accept file
+        // cb{undefined, false}                             //reject file without throwing error
+    }
+})
+router.post('/users/me/avatar', auth, upload.single('avatar'), async (req, res)=>{
+    const buffer = await sharp(req.file.buffer).resize({ width:250, height:250 }).png().toBuffer()                     //resize the avatar image, convert to png and return the result as buffer
+    req.user.avatar = buffer                              //req.file.buffer contains the file received by multer
+    await req.user.save()
+    res.status(200).send()
+}, (error, req, res, next)=>{                                   //This function tells express to call it when an error has occurred instead of sending out long irrelevant errors and it should have exactly those 4 params
+    res.status(400).send({Error : error.message})              //error.message will show whatever error we throw in our multer middleware
+})
+
+router.delete('/users/me/avatar', auth, async(req, res)=>{
+    try{
+    req.user.avatar = undefined
+    await req.user.save()
+    res.status(200).send()
+    } catch(error){
+        console.log(error)
+        res.status(400).send()
+    }
+})
+
+router.get('/users/:id/avatar', async(req, res)=>{
+    try{
+        const user = await User.findById(req.params.id)
+        if(!user || !user.avatar) throw new Error()
+
+        res.set('Content-Type', 'image/png')                         //Setting response header for the image type
+        res.status(200).send(user.avatar)
+
+    } catch(error){
+        res.status(404).send()
     }
 })
 
